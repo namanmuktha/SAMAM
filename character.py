1f0c6c5b1c493d49f0390f0ce6480312908f428d
naman muktha <namankumarmuktha@gmail.com>
2023-12-30 18:26:25 +0530

    matplot lib removed

--- character.py
+++ character.py
@@ -6,7 +6,6 @@
 
 import numpy as np 
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
